3a3e61551dd099e7d06d2d17618815ce0e650a16
dhseol <seol83@gmail.com>
2020-01-10 16:11:32 +0900

    junit 테스트 수정

--- src/test/java/com/dhstudy/webservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/dhstudy/webservice/domain/PostsRepositoryTest.java
@@ -2,31 +2,32 @@ package com.dhstudy.webservice.domain;
 
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
 @SpringBootTest
-public class PostsRepositoryTest {
+class PostsRepositoryTest {
+
     @Autowired
     PostsRepository postsRepository;
 
-    @AfterAll
+    @AfterEach
     public void cleanup(){
-        /**
-         * 이후 테스트 코드에 영향을 끼치지 않기 위해
-         * 테스트 메소드가 끝날때 마다 repository 전체 비우는 코드
-         */
-
-         postsRepository.deleteAll();
+        // 이후 테스트 코드에 영향을 끼치지 않기 위해
+        // 테스트 메소드가 끝날때 마다 repository 전체 비우는 코드
+        
+        postsRepository.deleteAll();        
     }
 
-    @Test
-    public void 게시글저장_불러오기(){
+
+	@Test
+	void 게시글저장_불러오기() {
+
         //given
         postsRepository.save(Posts.builder()
         .title("테스트 게시글")
@@ -41,5 +42,7 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
-    }
+
+	}
+
 }
