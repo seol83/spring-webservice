57746d754fb3d13500b917acb37b541ced9d64ea
dhseol <seol83@gmail.com>
2020-01-14 16:30:34 +0900

    Handlebars setting

---
+++ src/test/java/com/dhstudy/webservice/web/WebControllerTest.java
@@ -0,0 +1,26 @@
+package com.dhstudy.webservice.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class WebControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹서비스");
+    }
+}
