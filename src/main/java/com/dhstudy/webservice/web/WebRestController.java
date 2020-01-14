6753d573971563e95a2a7294f2b82014daa1fdf0
dhseol <seol83@gmail.com>
2020-01-14 10:33:47 +0900

    commit work

--- src/main/java/com/dhstudy/webservice/web/WebRestController.java
+++ src/main/java/com/dhstudy/webservice/web/WebRestController.java
@@ -1,13 +1,31 @@
 package com.dhstudy.webservice.web;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import com.dhstudy.webservice.domain.PostsRepository;
+import com.dhstudy.webservice.dto.PostsSaveRequestDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping(value="/hello")
     public String hello() {
         return "HelloWorld!";
     }
+
+    @PostMapping(value="/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
+    
 }
