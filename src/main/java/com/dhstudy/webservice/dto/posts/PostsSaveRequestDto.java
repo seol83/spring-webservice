d2076ee444b9d44242409c1b0a4a1551fc04ee01
설대희 <dhseol@kuwoo.co.kr>
2020-01-21 18:35:31 +0900

    upload resouce

---
+++ src/main/java/com/dhstudy/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,20 @@
+package com.dhstudy.webservice.dto.posts;
+
+import com.dhstudy.webservice.domain.Posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder().title(title).content(content).author(author).build();
+    }
+}
